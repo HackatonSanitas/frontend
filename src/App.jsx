a60e87367a307c4b9534a7a195cde86cb9b07159
AngelaBello-creator <angelabellomedina@gmail.com>
2025-09-18 11:44:49 +0200

    adding header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+function App() {
+  return (
+    <BrowserRouter>
+    
+      <Header />
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
